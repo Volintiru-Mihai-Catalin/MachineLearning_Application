edc43678c29f494c2b2f8dec9444810d55d0955f
Sebastian-Daniel Staretu <125968790+daniel-staretu@users.noreply.github.com>
2023-07-19 13:52:18 +0300

    added training data separator function

--- ApplicationML/csv_separator.py
+++ ApplicationML/csv_separator.py
@@ -2,12 +2,11 @@ import pandas as pd
 
 def csv_separator(file):
     source_df = pd.read_csv(file)
+    #Put 70% of the source data in the training dataframe
     training_df = source_df.sample(frac=70/100)
     
+    #Take the remaining data and put it in the testing dataframe
     merged = pd.merge(source_df, training_df, how='left', left_index=True, right_index=True, indicator=True)
     testing_df = merged[merged['_merge'] == 'left_only'].drop('_merge', axis=1)
     
-    testing_df.to_csv("testing_df.csv")
-    training_df.to_csv("training_df.csv")
-
-csv_separator("training_data.csv")
+    return training_df, testing_df
